cd25de1b8256ea27018436d49e1a3efc9777b547
Bartek <bartek.bak@protonmail.com>
2024-07-16 14:49:33 +0100

    Started moving some items over from original site

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,16 @@
 <script>
-
+    import Sidebar from './Sidebar.svelte';
 </script>
 
 <main>
-
+    <Sidebar />
 </main>
 
 <style>
+  main {
+    height: 100dvh;
+  }
+  Sidebar {
+    height: 100%;
+  }
 </style>
---
+++ src/Sidebar.svelte
@@ -0,0 +1,31 @@
+<div class="sidebar-header">
+    <!-- svelte-ignore a11y-img-redundant-alt -->
+    <img
+      class="pfp"
+      src="./assets/newpfp.jpeg"
+      alt="profile picture of Bartosz Bak"
+    />
+</div>
+
+<div class="sidebar-nav">
+    <a class="sidebar-item" href="index.html">About</a>
+    <a class="sidebar-item" href="about.html">Projects</a>
+    <a class="sidebar-item" href="contact.html">Education</a>
+    <a class="sidebar-item" href="contact.html">Contact</a>
+    <a class="sidebar-item" href="contact.html">Official</a>
+</div>
+
+<style lang="scss">
+    .sidebar-header {
+        height: 35%;
+        display: flex;
+
+        align-items: flex-end;
+
+        img {
+            width: 6rem;
+            height: auto;
+            border-radius: 50%;
+        }
+    }
+</style>
