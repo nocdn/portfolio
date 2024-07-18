4168ada55fc6a531d17d1fdc22d86b1f2be73efb
Bartek <bartek.bak@protonmail.com>
2024-07-18 15:01:16 +0100

    Started the projects page

---
+++ src/Projects.svelte
@@ -0,0 +1,10 @@
+<script>
+
+</script>
+
+<div class="projects-content content">
+    <div class="projects-header content-header">
+        <span class="markdown-text"># </span>
+        <h1 data-value="What have I made?">What have I made?</h1>
+    </div>
+</div>
