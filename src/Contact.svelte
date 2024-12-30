d7f5c9f1ec2fe007d92cba9778758a691a9081dd
Bartek Bak <bartek.bak@protonmail.com>
2024-12-30 19:00:40 +0000

    feat: started scramble effect

---
+++ src/Contact.svelte
@@ -0,0 +1,27 @@
+<script>
+  const phrases = [
+    "github",
+    "email",
+    "linkedin",
+    "phone",
+    "twitter",
+    "instagram",
+  ];
+
+  let changingString = $state(phrases[0]);
+
+  const chars = "!<>-_\\/[]{}—=+*^?#________";
+
+  function randomChar() {
+    return chars[Math.floor(Math.random() * chars.length)];
+  }
+</script>
+
+<div class="education-content content h-4/6">
+  <div class="education-header content-header">
+    <span class="markdown-text"># </span>
+    <h1 class="text-3xl" data-value="You can reach me on:">
+      You can reach me on: {changingString}
+    </h1>
+  </div>
+</div>
